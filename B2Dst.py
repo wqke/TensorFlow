9f300cfe3b3a46b6675416d902270570f5cf00d1
wqke <47601667+wqke@users.noreply.github.com>
2019-03-18 11:03:37 +0100

    Update B2Dst.py

--- B2Dst.py
+++ B2Dst.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 import sys, os
-
+import numpy as np
 sys.path.append("../")
 os.environ["CUDA_VISIBLE_DEVICES"] = ""   # Do not use GPU
 
@@ -15,35 +15,54 @@ if __name__ == "__main__" :
   phsp = tfa.FourBodyAngularPhaseSpace()
 
   # Fit parameters of the model 
+  """
   FL  = tfa.FitParameter("FL" ,  0.600,  0.000, 1.000, 0.01)    #Taken from Belle measurement
   AT2 = tfa.FitParameter("AT2",  0.200, -1.000, 1.000, 0.01)
   S5  = tfa.FitParameter("S5" , -0.100, -1.000, 1.000, 0.01)    #Initial guess
-
+  """
+  I8  = tfa.FitParameter("I8" ,  0.,  0.000, 1.000, 0.01)   
+  I7 = tfa.FitParameter("I7",  0., -1.000, 1.000, 0.01)
+  I6s  = tfa.FitParameter("I6s" , -0.250, -1.000, 1.000, 0.01) 
+  I6c  = tfa.FitParameter("I6c" ,  0.300,  0.000, 1.000, 0.01)    
+  I5 = tfa.FitParameter("I5",  0.350, 0.000, 1.000, 0.01)
+  I4  = tfa.FitParameter("I4" , -0.200, -1.000, 1.000, 0.01)  
+  I3  = tfa.FitParameter("I3" ,  -0.100,  -1.000, 1.000, 0.01)    
+  I2s = tfa.FitParameter("I2s",  0.100, -1.000, 1.000, 0.01)
+  I2c  = tfa.FitParameter("I2c" , -0.200, -1.000, 1.000, 0.01) 
+  I1s  = tfa.FitParameter("I1s" ,  0.450,  0.000, 1.000, 0.01)    
+  I1c = tfa.FitParameter("I1c",  0.550, 0.000, 1.000, 0.01)
+  I9  = tfa.FitParameter("I9" , -0.100, -1.000, 1.000, 0.01) 
   ### Start of model description
 
   def model(x) : 
     # Get phase space variables
-    cosThetaK = phsp.CosTheta1(x)
-    cosThetaL = phsp.CosTheta2(x)
-    phi = phsp.Phi(x)
+    cosThetast = phsp.CosTheta1(x)     #D* angle costhetast
+    cosThetal = phsp.CosTheta2(x)    #Lepton angle costhetal
+    chi = phsp.Phi(x)
 
     # Derived quantities
-    sinThetaK = tfa.Sqrt( 1.0 - cosThetaK * cosThetaK )
-    sinThetaL = tfa.Sqrt( 1.0 - cosThetaL * cosThetaL )
+    sinThetast = tfa.Sqrt( 1.0 - cosThetast * cosThetast )
+    sinThetal = tfa.Sqrt( 1.0 - cosThetal * cosThetal )
 
-    sinTheta2K =  (1.0 - cosThetaK * cosThetaK)
-    sinTheta2L =  (1.0 - cosThetaL * cosThetaL)
+    sinTheta2st =  (1.0 - cosThetast * cosThetast)
+    sinTheta2l =  (1.0 - cosThetal * cosThetal)
 
-    sin2ThetaK = (2.0 * sinThetaK * cosThetaK)
-    cos2ThetaL = (2.0 * cosThetaL * cosThetaL - 1.0)
+    sin2Thetast = (2.0 * sinThetast * cosThetast)
+    cos2Thetal = (2.0 * cosThetal * cosThetal - 1.0)
 
     # Decay density
-    pdf  = (3.0/4.0) * (1.0 - FL ) * sinTheta2K
-    pdf +=  FL * cosThetaK * cosThetaK
-    pdf +=  (1.0/4.0) * (1.0 - FL) * sin2ThetaK *  cos2ThetaL
-    pdf +=  (-1.0) * FL * cosThetaK * cosThetaK *  cos2ThetaL
-    pdf +=  (1.0/2.0) * (1.0 - FL) * AT2 * sinTheta2K * sinTheta2L * tfa.Cos(2.0 * phi )
-    pdf +=  S5 * sin2ThetaK * sinThetaL * tfa.Cos( phi )
+    pdf  = (9.0/(32.np.pi)) * I1c* cosThetast*cosThetast
+    pdf +=  (9.0/(32.np.pi)) * I1s * sinTheta2st
+    pdf +=  (9.0/(32.np.pi)) * I2c * cosThetast*cosThetast*cos2Thetal
+    pdf +=  (9.0/(32.np.pi)) * I2s * sinTheta2st *  cos2Thetal
+    pdf +=  (9.0/(32.np.pi))* I6c *cosThetast*cosThetast *cosThetal
+    pdf +=  (9.0/(32.np.pi))* I6s * sinTheta2st *  cosThetal
+    pdf +=  (9.0/(32.np.pi))* I3 * np.cos(2*chi) * sinTheta2l * sinTheta2st
+    pdf +=  (9.0/(32.np.pi))* I9 * np.sin(2*chi) * sinTheta2l * sinTheta2st
+    pdf +=  (9.0/(32.np.pi))* I4 * np.cos(chi) * 2 * sinThetal * cosThetal * sin2Thetast 
+    pdf +=  (9.0/(32.np.pi))* I8 * np.sin(chi) * 2 * sinThetal * cosThetal * sin2Thetast 
+    pdf +=  (9.0/(32.np.pi))* I5 * np.cos(chi) * sinThetal  * sin2Thetast 
+    pdf +=  (9.0/(32.np.pi))* I7 * np.sin(chi) * sinThetal  * sin2Thetast 
 
     return pdf
 
