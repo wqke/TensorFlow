77920d5675e783a747927c21fb9223e76cc8faa2
wqke <47601667+wqke@users.noreply.github.com>
2019-03-27 15:54:09 +0100

    Update errorbars.py

--- errorbars.py
+++ errorbars.py
@@ -14,9 +14,29 @@ from matplotlib import cm,rc
 
 
 #Fedele prediction
-listI=[0,0,-1.38,1.96,-0.744,-0.563,0.346,-0.893,2.04,3.03,0]
-[I8,I7,I6s,I6c,I4 ,I3 ,I2s ,I2c ,I1s ,I1c ,I9]=[0/sum(listI),0/sum(listI),-1.38/sum(listI),1.96/sum(listI),-0.744/sum(listI),-0.563/sum(listI),0.346/sum(listI),-0.893/sum(listI),2.04/sum(listI),3.03/sum(listI),0/sum(listI)]
-[I8err,I7err,I6serr,I6cerr,I4err ,I3err ,I2serr ,I2cerr ,I1serr ,I1cerr ,I9]=[0/sum(listI),0/sum(listI),0.05/sum(listI),0.12/sum(listI),0.019/sum(listI),0.014/sum(listI),0.009/sum(listI),0.024/sum(listI),0.05/sum(listI),0.12/sum(listI),0/sum(listI)]
+
+vals = {'I1c':3.03,
+          'I1s':2.04,
+          'I2c':-0.89,
+          'I2s':0.35,
+          'I3': -0.56,
+          'I4':-0.74,
+          'I5': 1.61,
+          'I6c':1.96,
+          'I6s':-1.38,
+          'I7': 0.000,
+          'I8': 0.000,
+          'I9': 0.000}
+tot_rate = 0.
+for v in vals:
+  tot_rate += vals[v]
+#  tot_rate=vals["I1c"]
+for v in vals:
+  vals[v] = vals[v]/tot_rate
+
+
+[I8,I7,I6s,I6c,I4 ,I3 ,I2s ,I2c ,I1s ,I1c ,I9]=[vals["I8"],vals["I7"],vals["I6s"],vals["I6c"],vals["I4"] ,vals["I3"] ,vals["I2s"] ,vals["I2c"] ,vals["I1s"] ,vals["I1c"] ,vals["I9"]]
+[I8err,I7err,I6serr,I6cerr,I4err ,I3err ,I2serr ,I2cerr ,I1serr ,I1cerr ,I9]=[0/tot_rate,0/tot_rate,0.05/tot_rate,0.12/tot_rate,0.019/tot_rate,0.014/tot_rate,0.009/tot_rate,0.024/tot_rate,0.05/tot_rate,0.12/tot_rate,0/tot_rate]
 
 title_I6c=r"Unbinned fit for $I_{6c}$"
 title_I6s=r"Unbinned fit for $I_{6s}$"
@@ -47,7 +67,7 @@ def result(dec,geom,retrue,num):
 def xlist(n):
   return [n*2**(-0.1),n*2**(-0.05),n*2**(0.05),n*2**(0.1)]
 
-Xrange=[5,10,20,40,80]
+Xrange=[xlist(5)[0],10,20,40,xlist(80)[3]]
 
 plt.errorbar([xlist(5)[0],xlist(10)[0],xlist(20)[0],xlist(40)[0],xlist(80)[0]],
              [result("3pi","all","true","5")[0][3],result("3pi","all","true","10")[0][3],
@@ -92,7 +112,7 @@ plt.title(title_I6c)
 plt.xlabel("N (1000's)")
 plt.ylabel(r"$I_{6c}$")
 plt.xscale("log",basex=2.0)
-plt.xticks([5,10,20,40,80]) 
+plt.xticks([5,10,20,40,80],('5','10','20','40','80')) 
 plt.legend()
 
 plt.savefig('I6c.pdf')
