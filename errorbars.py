07ead42ea21ebfe9a36d367d7f8982e69fe64d69
wqke <47601667+wqke@users.noreply.github.com>
2019-03-27 16:39:28 +0100

    Update errorbars.py

--- errorbars.py
+++ errorbars.py
@@ -34,25 +34,23 @@ for v in vals:
 for v in vals:
   vals[v] = vals[v]/tot_rate
 
-
+Iname=["I8","I7","I6s","I6c","I4" ,"I3" ,"I2s" ,"I2c" ,"I1s" ,"I1c" ,"I9"]
 [I8,I7,I6s,I6c,I4 ,I3 ,I2s ,I2c ,I1s ,I1c ,I9]=[vals["I8"],vals["I7"],vals["I6s"],vals["I6c"],vals["I4"] ,vals["I3"] ,vals["I2s"] ,vals["I2c"] ,vals["I1s"] ,vals["I1c"] ,vals["I9"]]
 [I8err,I7err,I6serr,I6cerr,I4err ,I3err ,I2serr ,I2cerr ,I1serr ,I1cerr ,I9]=[0/tot_rate,0/tot_rate,0.05/tot_rate,0.12/tot_rate,0.019/tot_rate,0.014/tot_rate,0.009/tot_rate,0.024/tot_rate,0.05/tot_rate,0.12/tot_rate,0/tot_rate]
+Ilist=[I8,I7,I6s,I6c,I4 ,I3 ,I2s ,I2c ,I1s ,I1c ,I9]
+Ierrlist=[I8err,I7err,I6serr,I6cerr,I4err ,I3err ,I2serr ,I2cerr ,I1serr ,I1cerr ,I9]
 
-title_I6c=r"Unbinned fit for $I_{6c}$"
-title_I6s=r"Unbinned fit for $I_{6s}$"
-
-label1=r"3$\pi$-all-true"
-label2=r"3$\pi$-LHCb-reco"
-label3=r"3$\pi\pi_0$-all-true"
-label4=r"3$\pi\pi_0$-LHCb-reco"
+label1=r"3$\pi$-unbinned-all-true"
+label2=r"3$\pi$-binned-LHCb-true"
+label3=r"3$\pi$-binned-LHCb-reco"
 
 
 #I6c,3
 
 
 #define readfile 
-def result(dec,geom,retrue,num):
-  f=open("result_"+dec+"_"+geom+"_"+retrue+"_"+num+".txt", "r")
+def result(binned,dec,geom,retrue,num):
+  f=open("/home/ke/TensorFlowAnalysis/"+binned+"/result_"+dec+"_"+geom+"_"+retrue+"_"+num+".txt", "r")
   lines=f.readlines()
   result=[]
   err=[]
@@ -65,54 +63,49 @@ def result(dec,geom,retrue,num):
 
 ##End of readfile
 def xlist(n):
-  return [n*2**(-0.1),n*2**(-0.05),n*2**(0.05),n*2**(0.1)]
+  return [n*2**(-0.15),n,n*2**(0.15)]
 
-Xrange=[xlist(5)[0],10,20,40,xlist(80)[3]]
-
-plt.errorbar([xlist(5)[0],xlist(10)[0],xlist(20)[0],xlist(40)[0],xlist(80)[0]],
-             [result("3pi","all","true","5")[0][3],result("3pi","all","true","10")[0][3],
-            result("3pi","all","true","20")[0][3],result("3pi","all","true","40")[0][3],
-              result("3pi","all","true","80")[0][3]],
-             yerr=[result("3pi","all","true","5")[1][3],
-             result("3pi","all","true","10")[1][3],result("3pi","all","true","20")[1][3],
-             result("3pi","all","true","40")[1][3],result("3pi","all","true","80")[1][3]], fmt='o', color='black',
-ecolor='#6059f7', elinewidth=3, capsize=0,label=label1)
-
-plt.errorbar([xlist(5)[1],xlist(10)[1],xlist(20)[1],xlist(40)[1],xlist(80)[1]],
-             [result("3pi","LHCb","reco","5")[0][3],result("3pi","LHCb","reco","10")[0][3],
-            result("3pi","LHCb","reco","20")[0][3],result("3pi","LHCb","reco","40")[0][3],
-              result("3pi","LHCb","reco","80")[0][3]],
-             yerr=[result("3pi","LHCb","reco","5")[1][3],
-             result("3pi","LHCb","reco","10")[1][3],result("3pi","LHCb","reco","20")[1][3],
-             result("3pi","LHCb","reco","40")[1][3],result("3pi","LHCb","reco","80")[1][3]], fmt='o', color='black',
-ecolor='#f2a026', elinewidth=3, capsize=0,label=label2)
-
-plt.errorbar([xlist(5)[2],xlist(10)[2],xlist(20)[2],xlist(40)[2],xlist(80)[2]],
-             [result("3pipi0","all","true","5")[0][3],result("3pipi0","all","true","10")[0][3],
-            result("3pipi0","all","true","20")[0][3],result("3pipi0","all","true","40")[0][3],
-              result("3pipi0","all","true","80")[0][3]],
-             yerr=[result("3pipi0","all","true","5")[1][3],
-             result("3pipi0","all","true","10")[1][3],result("3pipi0","all","true","20")[1][3],
-             result("3pipi0","all","true","40")[1][3],result("3pipi0","all","true","80")[1][3]], fmt='o', color='black',
-ecolor='#d80622', elinewidth=3, capsize=0,label=label3)
-
-plt.errorbar([xlist(5)[3],xlist(10)[3],xlist(20)[3],xlist(40)[3],xlist(80)[3]],
-             [result("3pipi0","LHCb","reco","5")[0][3],result("3pipi0","LHCb","reco","10")[0][3],
-            result("3pipi0","LHCb","reco","20")[0][3],result("3pipi0","LHCb","reco","40")[0][3],
-              result("3pipi0","LHCb","reco","80")[0][3]],
-             yerr=[result("3pipi0","LHCb","reco","5")[1][3],
-             result("3pipi0","LHCb","reco","10")[1][3],result("3pipi0","LHCb","reco","20")[1][3],
-             result("3pipi0","LHCb","reco","40")[1][3],result("3pipi0","LHCb","reco","80")[1][3]], fmt='o', color='black',
-ecolor='#4e0366', elinewidth=3, capsize=0,label=label4)
+Xrange=[xlist(5)[0],10,20,40,xlist(80)[2]]
 
 
-plt.plot(Xrange,[I6c]*5,linestyle=':')
-plt.fill_between(Xrange,[I6c-I6cerr]*5 ,[I6c+I6cerr]*5 ,alpha=0.5,color='lightgray',label='Theory')
-plt.title(title_I6c)
-plt.xlabel("N (1000's)")
-plt.ylabel(r"$I_{6c}$")
-plt.xscale("log",basex=2.0)
-plt.xticks([5,10,20,40,80],('5','10','20','40','80')) 
-plt.legend()
+for i in range(11):
+          plt.errorbar([xlist(5)[0],xlist(10)[0],xlist(20)[0],xlist(40)[0],xlist(80)[0]],
+                       [result("UnbinnedResult","3pi","all","true","5")[0][i],result("UnbinnedResult","3pi","all","true","10")[0][i],
+                      result("UnbinnedResult","3pi","all","true","20")[0][i],result("UnbinnedResult","3pi","all","true","40")[0][i],
+                        result("UnbinnedResult","3pi","all","true","80")[0][i]],
+                       yerr=[result("UnbinnedResult","3pi","all","true","5")[1][i],
+                       result("UnbinnedResult","3pi","all","true","10")[1][i],result("UnbinnedResult","3pi","all","true","20")[1][i],
+                       result("UnbinnedResult","3pi","all","true","40")[1][i],result("UnbinnedResult","3pi","all","true","80")[1][i]], fmt='o', color='black',
+          ecolor='#6059f7', elinewidth=3, capsize=0,label=label1)
 
-plt.savefig('I6c.pdf')
+          plt.errorbar([xlist(5)[1],xlist(10)[1],xlist(20)[1],xlist(40)[1],xlist(80)[1]],
+                       [result("BinnedResult","3pi","LHCb","true","5")[0][i],result("BinnedResult","3pi","LHCb","true","10")[0][i],
+                      result("BinnedResult","3pi","LHCb","true","20")[0][i],result("BinnedResult","3pi","LHCb","true","40")[0][i],
+                        result("BinnedResult","3pi","LHCb","true","80")[0][i]],
+                       yerr=[result("BinnedResult","3pi","LHCb","true","5")[1][i],
+                       result("BinnedResult","3pi","LHCb","true","10")[1][i],result("BinnedResult","3pi","LHCb","true","20")[1][i],
+                       result("BinnedResult","3pi","LHCb","true","40")[1][i],result("BinnedResult","3pi","LHCb","true","80")[1][i]], fmt='o', color='black',
+          ecolor='#f2a026', elinewidth=3, capsize=0,label=label2)
+          
+          plt.errorbar([xlist(5)[2],xlist(10)[2],xlist(20)[2],xlist(40)[2],xlist(80)[2]],
+                       [result("BinnedResult","3pi","LHCb","reco","5")[0][i],result("BinnedResult","3pi","LHCb","reco","10")[0][i],
+                      result("BinnedResult","3pi","LHCb","reco","20")[0][i],result("BinnedResult","3pi","LHCb","reco","40")[0][i],
+                        result("BinnedResult","3pi","LHCb","reco","80")[0][i]],
+                       yerr=[result("BinnedResult","3pi","LHCb","reco","5")[1][i],
+                       result("BinnedResult","3pi","LHCb","reco","10")[1][i],result("BinnedResult","3pi","LHCb","reco","20")[1][i],
+                       result("BinnedResult","3pi","LHCb","reco","40")[1][i],result("BinnedResult","3pi","LHCb","reco","80")[1][i]], fmt='o', color='black',
+          ecolor='#960311', elinewidth=3, capsize=0,label=label3)
+          plt.plot(Xrange,[Ilist[i]]*5,linestyle=':')
+          plt.fill_between(Xrange,[Ilist[i]-Ierrlist[i]]*5 ,[Ilist[i]+Ierrlist[i]]*5 ,alpha=0.5,color='lightgray',label='Theory')
+          plt.title(r"Fit results for "+Iname[i])
+          plt.xlabel("N (1000's)")
+          plt.ylabel(Iname[i])
+          plt.xscale("log",basex=2.0)
+          plt.xticks([5,10,20,40,80],('5','10','20','40','80')) 
+          plt.legend()
+          plt.savefig(Iname[i]+'.pdf')
+          plt.close()
+          plt.close()
+          plt.close()
+          plt.close()
+          plt.close()
