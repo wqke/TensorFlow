17af74bfa3f751f7ae9a354599fc1505e09db5f3
wqke <47601667+wqke@users.noreply.github.com>
2019-04-29 17:18:41 +0200

    Update Classifier.py

--- Classifier.py
+++ Classifier.py
@@ -2,6 +2,7 @@ import matplotlib
 matplotlib.use('Agg')
 from matplotlib import pyplot as plt
 from root_numpy import root2array, rec2array,array2root
+from sklearn import cross_validation
 from sklearn.metrics import accuracy_score, log_loss, classification_report, roc_auc_score,confusion_matrix
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC, LinearSVC, NuSVC
@@ -11,9 +12,8 @@ import pandas as pd
 import numpy as np
 from root_pandas import *
 import sys, os, math
-sys.path.append("../../TensorFlowAnalysis")
 os.environ["CUDA_VISIBLE_DEVICES"] = ""   # Do not use GPU
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import  train_test_split,KFold
 
 
 import pandas.core.common as com
@@ -23,73 +23,29 @@ from pandas.tools import plotting
 from pandas.tools.plotting import scatter_matrix
 
 
-branch_names=[ 'Tau_FD_z',  'Tau_E', '3pi_M', '3pi_PZ', 'Tau_m12', 'Tau_m13','Tau_m23',
-         'Tau_FD', 'costheta_D_reco','costheta_L_reco','q2_reco','Tau_PZ_reco',
-'chi_reco', 'Tau_life_reco']
+branch_names=['3pi_M',  'Tau_m12', 'Tau_m13','Tau_m23','Tau_FD','Tau_life_reco']
 
 
 if __name__ == "__main__" :
 
   #Read RapidSim signal sample for either 3pi mode or 3pipi0 mode
   mode = "Bd2DstTauNu"
-  #3pi or 3pipi0
-  sub_mode = sys.argv[1]
-  #Geometry (all or LHCb)
-  geom = sys.argv[2]
-  #True or reco angles
-  var_type = sys.argv[3]
-  #Number of events to run on (in k) - 5, 10, 20, 40, 80
-  num_sig = sys.argv[4]
-  #Hammer weight (SM / T1 / T2)
-  ham = sys.argv[5]
+  sub_mode = '3pi'
+  geom = 'LHCb'
+  var_type = 'reco'
+  ham = 'SM'
 
-  var_range = {"costheta_D_%s" % var_type: (-1.,1.),
-               "costheta_L_%s" % var_type: (-1.,1.),
-               "chi_%s" % var_type: (-math.pi,math.pi),
-               "Tau_life_%s" % var_type: (0.,3.0)
-  }  
+  var_range = {"Tau_life_%s" % var_type: (0.,3.0)}  
   
-
-  frac={}
-  M_B = 5.27963
-  M_Dst = 2.01026
-  
-  q2_max = (M_B - M_Dst)**2
-  q2_min = M_B - M_Dst
-  
-  if(var_type=="reco"):
-    q2_min = 0.0  
-  
-  #Fractions defined using Run 1 R(D*) fit
-  n_signal = 1296.
-  feed_frac = 0.11
-  n_feed = feed_frac*n_signal
-  n_ds = 6835.
-  n_d0 = 1.41 * 445.
-  n_dplus = 0.245 * n_ds
-  n_prompt = 424.
-  total_yield = n_signal + n_ds + n_dplus + n_feed + n_d0 + n_prompt
-
-  frac['signal'] = n_signal/total_yield  #floating
-  frac['Ds'] = n_ds/total_yield
-  frac['Dplus']= n_dplus/total_yield
-  frac['feed'] = n_feed/total_yield
-  frac['D0'] = n_d0/total_yield
-  frac['prompt'] = n_prompt/total_yield
-
-  print "Initial component fractions: %s" % frac
-
-  bkg_names=list(frac)
-  bkg_names.remove('signal')
   data_file_fit= "/data/lhcb/users/hill/Bd2DstTauNu_Angular/RapidSim_tuples/Bd2DstTauNu/%s_%s_Total/model_vars_weights_hammer.root" % (sub_mode,geom)
 
   data_sample_fit = read_root(data_file_fit,"DecayTree",columns=branch_names)
 
-  data_sample_fit = data_sample_fit.query("costheta_D_%s>=-1 and costheta_D_%s<=1 and costheta_L_%s>=-1 and costheta_L_%s<=1 and chi_%s>=-%s and chi_%s<=%s and Tau_life_%s > %s and Tau_life_%s <= %s and q2_%s > %s and q2_%s <= %s" % (var_type,var_type,var_type,var_type,var_type,math.pi,var_type,math.pi,var_type,var_range["Tau_life_%s" % var_type][0],var_type,var_range["Tau_life_%s" % var_type][1],var_type,q2_min,var_type,q2_max))
+  data_sample_fit = data_sample_fit.query("Tau_life_%s > %s and Tau_life_%s <= %s " % (var_type,var_range["Tau_life_%s" % var_type][0],var_type,var_range["Tau_life_%s" % var_type][1]))
   #data_sample_fit = data_sample_fit[branch_names]
 
   #Randomly sample down to required size
-  data_sample_fit = data_sample_fit.sample(n=int(num_sig)*1000,random_state=int(num_sig))
+  #data_sample_fit = data_sample_fit.sample(n=int(num_sig)*1000,random_state=int(num_sig))
   
   
   data_sample_fit.to_root("/home/ke/tmps/signal.root","DecayTree")
@@ -97,56 +53,54 @@ if __name__ == "__main__" :
   signal = rec2array(signal)
   
   
-  bkg_names.remove('signal')
+
+  bkg_names=['Ds','Dplus','D0']
   bkg_files={}
   for bkg in bkg_names:
-  	bkg_files[bkg] = "/data/lhcb/users/hill/Bd2DstTauNu_Angular/RapidSim_tuples/Merged_Bkg/%s.root" % bkg
-    
-    
-  background_sample_fit={}
-  for bkg in bkg_names:
-    background_sample_fit[bkg] = read_root(bkg_files[bkg],"DecayTree",columns=branch_names)
-    background_sample_fit[bkg] = background_sample_fit[bkg].query("costheta_D_%s>=-1 and costheta_D_%s<=1 and costheta_L_%s>=-1 and costheta_L_%s<=1 and chi_%s>=-%s and chi_%s<=%s and Tau_life_%s > %s and Tau_life_%s <= %s and q2_%s > %s and q2_%s <= %s" % (var_type,var_type,var_type,var_type,var_type,math.pi,var_type,math.pi,var_type,var_range["Tau_life_%s" % var_type][0],var_type,var_range["Tau_life_%s" % var_type][1],var_type,q2_min,var_type,q2_max))
-    background_sample_fit[bkg] = background_sample_fit[bkg][branch_names]
-    background_sample_fit[bkg] = background_sample_fit[bkg].sample(n=int(len(data_sample_fit)*(frac[bkg]/frac['signal'])),random_state=int(num_sig))
+    bkg_files[bkg] = "/data/lhcb/users/hill/Bd2DstTauNu_Angular/RapidSim_tuples/Merged_Bkg/%s.root" % bkg
+
+
+  #keep the same number of D backgrounds  (equal to Dplus numbers)
+  background_sample_fit={}     
+  background_sample_fit['Dplus']= read_root(bkg_files['Dplus'],"DecayTree",columns=branch_names)     
+  background_sample_fit['Dplus'] = background_sample_fit['Dplus'].query("Tau_life_%s > %s and Tau_life_%s <= %s " % (var_type,var_range["Tau_life_%s" % var_type][0],var_type,var_range["Tau_life_%s" % var_type][1]))
+
+  sample_length=len(background_sample_fit['Dplus'])       
+  background_sample_fit['Ds']= read_root(bkg_files['Ds'],"DecayTree",columns=branch_names)      
+  background_sample_fit['Ds'] = background_sample_fit['Ds'].query("Tau_life_%s > %s and Tau_life_%s <= %s " % (var_type,var_range["Tau_life_%s" % var_type][0],var_type,var_range["Tau_life_%s" % var_type][1]))
+  background_sample_fit['Ds'] = background_sample_fit['Ds'].sample(n=int(sample_length),random_state=int(sample_length/1000.))
+
+  background_sample_fit['D0']= read_root(bkg_files['D0'],"DecayTree",columns=branch_names)      
+  background_sample_fit['D0'] = background_sample_fit['D0'].query("Tau_life_%s > %s and Tau_life_%s <= %s " % (var_type,var_range["Tau_life_%s" % var_type][0],var_type,var_range["Tau_life_%s" % var_type][1]))
+  background_sample_fit['D0'] = background_sample_fit['D0'].sample(n=int(sample_length),random_state=int(sample_length/1000.))
 
   bkg_samp=background_sample_fit[bkg_names[0]]
   for bkg in bkg_names[1:] :   
     bkg_samp = pd.concat([bkg_samp,background_sample_fit[bkg]], ignore_index=True)
-    
-#  #Add backgrounds to the signal
-#  for bkg in bkg_names:
-#    data_sample_fit = pd.concat([data_sample_fit,background_sample_fit[bkg]], ignore_index=True)
 
-  
+
+#  #Add backgrounds to the signal
+
   bkg_samp.to_root("/home/ke/tmps/bkg.root","DecayTree")
   backgr = root2array("/home/ke/tmps/bkg.root","DecayTree",branch_names)
   backgr = rec2array(backgr)
-    
-
 
   # for sklearn data is usually organised
   # into one 2D array of shape (n_samples x n_features)
   # containing all the data and one array of categories
   # of length n_samples
   X = np.concatenate((signal, backgr))
-  y = np.concatenate((np.ones(signal.shape[0]),
-                      np.zeros(backgr.shape[0])))     #signal=1,bkg=0
+  y = np.concatenate((np.ones(signal.shape[0]),np.zeros(backgr.shape[0])))     #signal=1,bkg=0
     
   #df = pd.DataFrame(np.hstack((X, y.reshape(y.shape[0], -1))),columns=branch_names+['y'])  
   
     
-  X_dev,X_eval, y_dev,y_eval = train_test_split(X, y,
-                                              test_size=0.33, random_state=42)
-  X_train,X_test, y_train,y_test = train_test_split(X_dev, y_dev,
-                                                  test_size=0.33, random_state=492)  
+  X_dev,X_eval, y_dev,y_eval = train_test_split(X, y,test_size=0.33, random_state=42)
+  X_train,X_test, y_train,y_test = train_test_split(X_dev, y_dev,test_size=0.33, random_state=492)  
   
-  
-  
-  
-
-  bdt = GradientBoostingClassifier(n_estimators=20, learning_rate = 0.5, max_features=len(branch_names), max_depth = 2, random_state = 0)
-
+  bdt = GradientBoostingClassifier(n_estimators=200,max_depth=1,learning_rate=0.5)
+#sub_sample=0.5
+                                                                   
   bdt.fit(X_train, y_train)
 
 
@@ -193,3 +147,19 @@ if __name__ == "__main__" :
     
     
   compare_train_test(bdt, X_train, y_train, X_test, y_test)
+
+  y_predicted = bdt.decision_function(X)
+  y_predicted.dtype = [('y', np.float64)]
+
+  y_predicted = bdt.predict(X_test)
+  print classification_report(y_test, y_predicted,
+                              target_names=["background", "signal"])
+  print "Area under ROC curve: %.4f"%(roc_auc_score(y_test,
+                                                  bdt.decision_function(X_test)))
+
+
+  
+  y_predicted = bdt.decision_function(X)
+  y_predicted.dtype = [('y', np.float64)]
+  array2root(y_predicted, "/home/ke/tmps/test-prediction.root", "BDToutput")
+
